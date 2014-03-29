848862a71250b50c5f6df657b1502103592b4469
liuzhengyi <lzy@lzy-x201i.(none)>
2014-03-29 21:18:40 +0800

    后台 basic

---
+++ admin/action/edit_article_do.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * action: update an adver
+ * gipsaliu@gmail.com
+ * since: 2014-03-29
+ */
+
+include_once('../../config.php');
+
+require_once("../../lib/common.php");
+require_once($_cfg_dbConfFile);
+
+// TODO permission control
+
+
+// get params
+if ( empty($_POST['id']) || empty($_POST['title']) || empty($_POST['cate_id']) ) {
+var_dump($_POST);
+    output_json_error(-10001, '必填参数不全');
+}
+
+$params['title']    = strval($_POST['title']);
+$params['id']       = intval($_POST['id']);
+$params['cate_id']  = intval($_POST['cate_id']);
+$params['type']     = empty($_POST['type'])     ? '' : intval($_POST['type']);
+$params['subtitle'] = empty($_POST['subtitle']) ? '' : strval($_POST['subtitle']);
+$params['abstract'] = empty($_POST['abstract']) ? '' : strval($_POST['abstract']);
+$params['remark']   = empty($_POST['remark'])   ? '' : strval($_POST['remark']);
+$params['content']  = empty($_POST['content'])  ? '' : strval($_POST['content']);
+
+
+$dbh    = new PDO($_cfg_db_dsn, $_cfg_db_user, $_cfg_db_pwd);
+$sql    = ' update article set
+                title = :title,
+                cate_id = :cate_id,
+                type = :type,
+                subtitle = :subtitle,
+                abstract = :abstract,
+                remark = :remark,
+                content=:content
+            where
+                article_id = :id
+            limit 1';
+$sth    = $dbh->prepare($sql);
+
+$sth->bindParam(':title',       $params['title'],       PDO::PARAM_STR);
+$sth->bindParam(':cate_id',     $params['cate_id'],     PDO::PARAM_INT);
+$sth->bindParam(':type',        $params['type'],        PDO::PARAM_INT);
+$sth->bindParam(':subtitle',    $params['subtitle'],    PDO::PARAM_STR);
+$sth->bindParam(':abstract',    $params['abstract'],    PDO::PARAM_STR);
+$sth->bindParam(':remark',      $params['remark'],      PDO::PARAM_STR);
+$sth->bindParam(':content',     $params['content'],     PDO::PARAM_STR);
+$sth->bindParam(':id',          $params['id'],          PDO::PARAM_INT);
+
+$result = $sth->execute();
+
+if ( FALSE === $result ) {
+    output_json_error(-10002, '修改失败');
+}
+
+output_json_info('修改成功');
+
+?>
---
+++ admin/action/edit_house_do.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * action: update an house
+ * gipsaliu@gmail.com
+ * since: 2014-03-29
+ */
+
+//var_dump($_POST);
+//var_dump($_FILES); exit();
+
+include_once('../../config.php');
+
+require_once("../../lib/common.php");
+require_once($_cfg_dbConfFile);
+
+// TODO permission control
+
+// get params
+if (    empty($_POST['id']) || empty($_POST['city_id']) ||
+        empty($_POST['name']) || empty($_POST['price_desc']) ||
+        empty($_POST['image_urls'])
+   ) {
+    output_json_error(-10001, '必填参数: city_id, name, price_desc, image_urls 不全!');
+}
+
+
+// upload image
+if ( ! empty($_FILES) ) {
+    $new_images = array();
+    for ( $index = 0; $index < 4; ++$index ) {
+        $name = 'house_img_'. $index;
+        if ( ! empty($_FILES[$name]['name']) ) {
+            $new_images[$name] = $_FILES[$name];
+        } else {
+            $new_images[$name] = NULL;
+        }
+    }
+}
+
+// var_dump($new_images); // TODO upload images
+
+
+// filter params
+$params['id']                   = intval($_POST['id']);
+$params['city_id']              = strval($_POST['city_id']);
+$params['name']                 = strval($_POST['name']);
+$params['price_desc']           = strval($_POST['price_desc']);
+$params['image']                = strval($_POST['image_urls']);
+$params['type']                 = empty($_POST['type']) ? '' : intval($_POST['type']);
+$params['layout_area']          = empty($_POST['layout_area']) ? '' : strval($_POST['layout_area']);
+$params['price_level']          = empty($_POST['price_level']) ? '' : intval($_POST['price_level']);
+$params['position']             = empty($_POST['position']) ? '' : strval($_POST['position']);
+$params['decoration_state']     = empty($_POST['decoration_state']) ? '' : strval($_POST['decoration_state']);
+$params['property']             = empty($_POST['property']) ? '' : strval($_POST['property']);
+$params['project_intro_brief']  = empty($_POST['project_intro_brief']) ? '' : strval($_POST['project_intro_brief']);
+$params['project_intro']        = empty($_POST['project_intro']) ? '' : strval($_POST['project_intro']);
+$params['phone_num']            = empty($_POST['phone_num']) ? '' : strval($_POST['phone_num']);
+$params['is_on_sale']           = empty($_POST['is_on_sale']) ? '' : intval($_POST['is_on_sale']);
+$params['is_rental']            = empty($_POST['is_rental']) ? '' : intval($_POST['is_rental']);
+$params['remark']               = empty($_POST['remark']) ? '' : strval($_POST['remark']);
+
+
+$dbh    = new PDO($_cfg_db_dsn, $_cfg_db_user, $_cfg_db_pwd);
+$sql    = ' update house set
+                city_id = :city_id,
+                name = :name,
+                price_desc = :price_desc,
+                image_urls = :image,
+                type = :type,
+                layout_area = :layout_area,
+                price_level = :price_level,
+                position = :position,
+                decoration_state = :decoration_state,
+                property = :property,
+                project_intro_brief = :project_intro_brief,
+                project_intro = :project_intro,
+                phone_num = :phone_num,
+                is_on_sale = :is_on_sale,
+                is_rental = :is_rental,
+                remark = :remark
+            where
+                house_id = :id
+            limit 1';
+$sth    = $dbh->prepare($sql);
+
+$sth->bindParam(':id', $params['id'], PDO::PARAM_INT);
+$sth->bindParam(':city_id', $params['city_id'], PDO::PARAM_INT);
+$sth->bindParam(':name', $params['name'], PDO::PARAM_STR);
+$sth->bindParam(':price_desc', $params['price_desc'], PDO::PARAM_STR);
+$sth->bindParam(':image', $params['image'], PDO::PARAM_STR);
+$sth->bindParam(':type', $params['type'], PDO::PARAM_INT);
+$sth->bindParam(':layout_area', $params['layout_area'], PDO::PARAM_STR);
+$sth->bindParam(':price_level', $params['price_level'], PDO::PARAM_INT);
+$sth->bindParam(':position', $params['position'], PDO::PARAM_STR);
+$sth->bindParam(':decoration_state', $params['decoration_state'], PDO::PARAM_STR);
+$sth->bindParam(':property', $params['property'], PDO::PARAM_STR);
+$sth->bindParam(':project_intro_brief', $params['project_intro_brief'], PDO::PARAM_STR);
+$sth->bindParam(':project_intro', $params['project_intro'], PDO::PARAM_STR);
+$sth->bindParam(':phone_num', $params['phone_num'], PDO::PARAM_STR);
+$sth->bindParam(':is_on_sale', $params['is_on_sale'], PDO::PARAM_INT);
+$sth->bindParam(':is_rental', $params['is_rental'], PDO::PARAM_INT);
+$sth->bindParam(':remark', $params['remark'], PDO::PARAM_STR);
+
+$result = $sth->execute();
+
+if ( FALSE === $result ) {
+    var_dump($sth->errorInfo());
+    output_json_error(-10002, '修改失败');
+}
+
+output_json_info('修改成功');
+
+?>
