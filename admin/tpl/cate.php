71279147ff3b3d9bfcdeca2f6fe6fffd3e1804ed
liuzhengyi <lzy@lzy-x201i.(none)>
2014-03-30 11:09:34 +0800

    添加3个入口页面

--- admin/tpl/cate.php
+++ admin/tpl/cate.php
@@ -3,7 +3,7 @@
 <div id="main-container">
 <h2>文章分类管理</h2>
 <p><a href="#">添加分类</a></p>
-<table id="ad-manage" border="1" >
+<table id="cate-manage" border="1" >
     <thead>
         <tr>
             <th id="">ID</th>
@@ -15,21 +15,13 @@
     <tbody>
         <?php
         foreach ( $cate_data as $cate ) {
-            $img_url    = $_cfg_siteRootAdmin. '/uploads/'. get_single_img_url($house['image_urls']);
-            $link_url   = $_cfg_siteRoot. 'house.php?id='. $house['house_id'];
-            $edit_url   = $_cfg_siteRootAdmin. '/edit.php?content=house&id='. $house['house_id'];
+            $edit_url   = $_cfg_siteRootAdmin. '/edit.php?content=cate&id='. $cate['cate_id'];
             $del_url    = 'unfinished';
 
             echo '<tr>';
-            echo '<td>'. $house['house_id']. '</td>';
-            echo '<td>'. $house['ciname']. '</td>';
-            echo '<td><a href="'. $link_url. '">'. $house['name']. '</a></td>';
-            echo '<td>'. $house['type']. '</td>';
-            echo '<td><a href="'. $img_url. '"><img src="'. $img_url. '" /></a></td>';
-            echo '<td>'. $house['layout_area']. '</td>';
-            echo '<td>'. $house['price_desc']. '</td>';
-            echo '<td>'. $house['position']. '</td>';
-            echo '<td>'. $house['status']. '</td>';
+            echo '<td>'. $cate['cate_id']. '</td>';
+            echo '<td>'. $cate['position']. '</td>';
+            echo '<td>'. $cate['status']. '</td>';
             echo '<td><a href="#">删除(un)</a> <a href="'. $edit_url. '">修改</a> </td>';
             echo '</tr>';
         }
